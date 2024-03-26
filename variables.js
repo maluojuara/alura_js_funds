7a699bfe3bcc8b295655ee781d65e4d93ae47f54
maluojuara <araujomaluc@gmail.com>
2024-03-26 12:16:54 -0300

    learning variables

---
+++ variables.js
@@ -0,0 +1,18 @@
+let student = 'Caroline';
+let teacher = 'Ana';
+
+console.log(student, teacher);
+
+student = 'Mariana';
+
+console.log(student, teacher);
+
+
+// Use const to avoid changes
+const student_a = 'Caroline';
+
+// student_a = 'Mariana'; - can't change a const
+
+console.log(student_a);
+
+// Returns an error (TypeError)
