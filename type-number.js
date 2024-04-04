c454824d26f24c286519f1f0b260efd5ed0e77de
maluojuara <araujomaluc@gmail.com>
2024-04-04 15:51:19 -0300

    data types - number

---
+++ type-number.js
@@ -0,0 +1,31 @@
+const grade1stSem = 8;
+const grade2ndSem = 6.3;
+
+
+let total = grade1stSem + grade2ndSem;
+console.log(total);
+
+const grade3rdSem = 2;
+// const grade4thSem = '5'; // it's not gonna be an error (concatenate) - weakly typed
+
+// total = grade1stSem + grade2ndSem + grade3rdSem + grade4thSem;
+
+// console.log(total);
+
+// ---------------------------------------------
+
+const myName = "Malu";
+console.log("my  name is " + myName);
+
+
+// ---------------------------------------------
+
+const grade4thSem = Number.parseInt('5'); //converts into an Int 
+
+total = grade1stSem + grade2ndSem + grade3rdSem + grade4thSem;
+
+const average = total / 4;
+
+console.log(total);
+console.log(average);
+console.log(average.toFixed(2)); // digits after comma 
