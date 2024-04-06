f19adeced4abc29ef8d997060f5993d403b27066
maluojuara <araujomaluc@gmail.com>
2024-04-06 18:01:23 -0300

    exercises class02 ex01

---
+++ exercises/class02/ex01.js
@@ -0,0 +1,7 @@
+// 01. Create a variable containing a sentence. Then, display in the console the length of the sentence and the same sentence in uppercase letters.
+
+const sentence = "Luke, I am your father!";
+
+console.log("The lenght of the sentence is: ", sentence.length);
+
+console.log("Uppercase sentence: ", sentence.toUpperCase());
