9e55ed50cd229a7d8ea90708b4e5d9d2232904f7
maluojuara <araujomaluc@gmail.com>
2024-04-07 23:17:36 -0300

    exercises class02

---
+++ exercises/class02/ex03.js
@@ -0,0 +1,7 @@
+//03. Create 3 variables of different types, use template strings to combine the different data types (number, string, boolean) into a single string, and display the result in the console
+
+const var_string = "hello";
+const var_number = 42;
+const var_boolean = true;
+
+console.log(`${var_string}, I am ${var_number} years old. Is that ${var_boolean}?`);
